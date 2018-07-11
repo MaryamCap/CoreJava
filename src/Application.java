0bdc85e89d700d1f2e4733de9b48ea28d9dea3d9
Maryam <moheb5159@gmail.com>
2018-07-11 00:14:12 -0500

    Project-1

---
+++ src/Application.java
@@ -0,0 +1,28 @@
+
+public class Application {
+	
+
+
+	public static void main(String[] args) {
+		int  myNumber= 88;
+		short myShort = 847;
+		long myLong = 9797; 
+		double myDouble = 7.3526;
+		float myFloat = 325.658f;
+		char myChar = 'y';
+		boolean myBoolean = false;
+		byte myByte = 127;
+		
+		System.err.println("Hello World");
+		System.out.println (myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+
+	}
+
+}
